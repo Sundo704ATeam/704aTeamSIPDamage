5153c4c6862d26f5b0917a48cdc05e6d22d9c5b4
psw990802 <samuel3434@naver.com>
2025-09-11 19:30:28 +0900

    9/11 dust진행중... (중간push)

---
+++ src/main/java/egovframework/sipdamage704a/service/DustService.java
@@ -0,0 +1,11 @@
+package egovframework.sipdamage704a.service;
+
+import java.util.List;
+
+import egovframework.sipdamage704a.dto.dust.DustStationDto;
+
+public interface DustService {
+
+	List<DustStationDto> 		getDustStation();
+
+}
